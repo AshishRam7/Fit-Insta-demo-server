0b355ae9c798ae0487f15bd3b0709f94624fc8b2
AshishRam7 <jaashishram7@gmail.com>
2025-02-24 15:42:34 +0530

    modified requirments

--- server.py
+++ server.py
@@ -18,6 +18,7 @@ from dotenv import load_dotenv
 import httpx  # Using httpx for async requests
 from nltk.sentiment import SentimentIntensityAnalyzer
 import nltk
+import psycopg2
 
 from celery import Celery
 import random
